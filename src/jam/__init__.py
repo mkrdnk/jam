476ec439107896d9274d4ee484310697ecd3346a
Adrian Makridenko <adrianmakridenko@duck.com>
2026-09-15 12:05:57 +0300

    [^] v4.0.0b8

--- src/jam/__init__.py
+++ src/jam/__init__.py
@@ -24,7 +24,7 @@ logging.getLogger("jam").addHandler(logging.NullHandler())
 logging.getLogger("jam").addFilter(SensitiveDataFilter())
 
 
-__version__ = "4.0.0b7"
+__version__ = "4.0.0b8"
 __all__ = [
     "Jam",
     "BaseJam",
